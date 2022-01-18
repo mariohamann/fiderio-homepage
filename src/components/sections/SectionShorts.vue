8c2b48e805c144c4c82c4d879c18e9c4d2119e2d
Mario Hamann <mariovhamann@gmail.com>
2022-01-18 20:35:29 +0100

    fix: licensed image

--- src/components/sections/SectionShorts.vue
+++ src/components/sections/SectionShorts.vue
@@ -53,7 +53,7 @@
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
         <Card>
           <template #icon>
-            <IconExport />
+            <IconEquitations />
           </template>
           <template #title>
             Equations
